f1f5a5f3a63e1d6de72c69a7bd262896d993db22
TheFelixLive <jouloukas@gmail.com>
2024-11-03 14:16:38 +0100

    Updated help menu

--- Challenge Addon/scripts/main.js
+++ Challenge Addon/scripts/main.js
@@ -7,174 +7,162 @@ let oldLevel = 0;
 let latest_y = 999;
 let previousDimension = null;
 
-let is_available = 1;
-
 function mainTick() {
+  const players = world.getAllPlayers();
+  for (const player of players) {
+
+    
+    // This If query protects the script from crashing when the scoreboard "timer_settings" is deleted.
+    if (world.scoreboard.getObjective("timer_settings") !== undefined && world.scoreboard.getObjective("timer_addon") !== undefined) {
+      const x = Math.floor(player.location.x);
+      const z = Math.floor(player.location.z);
+
+      let level = player.level;
+      let reducedLevel = 0;
+      let xp_needed = player.totalXpNeededForNextLevel;
+
+      reducedLevel = oldLevel - level;
+      xp_needed = xp_needed - player.xpEarnedAtCurrentLevel;
 
 
-  // Decet a crash 
-  if (!timer_settings) {
-    is_available = 0;
-  }
+      if (level == undefined) continue;
 
-  
-  if (is_available == 1) {
-    const players = world.getAllPlayers();
-    for (const player of players) {
-        const x = Math.floor(player.location.x);
-        const z = Math.floor(player.location.z);
-  
-        let level = player.level;
-        let reducedLevel = 0;
-        let xp_needed = player.totalXpNeededForNextLevel;
-  
-        reducedLevel = oldLevel - level;
-        xp_needed = xp_needed - player.xpEarnedAtCurrentLevel;
-  
-  
-        if (level == undefined) continue;
-  
-        let newX = x;
-        let newZ = z;
-        let outsideBorder = false;
-  
-        if (x > level) {
-            newX = level;
-            outsideBorder = true;
+      let newX = x;
+      let newZ = z;
+      let outsideBorder = false;
+
+      if (x > level) {
+          newX = level;
+          outsideBorder = true;
+      }
+
+      else if (x < -level) {
+          newX = -level;
+          outsideBorder = true;
+      }
+
+      if (z > level) {
+          newZ = level;
+          outsideBorder = true;
+      }
+
+      else if (z < -level) {
+          newZ = -level;
+          outsideBorder = true;
+      }
+
+      // Only
+
+      const currentDimension = player.dimension.id; // Aktuelle Dimension des Spielers abrufen
+
+      // Überprüfen, ob es einen Dimensionswechsel gab
+      if (currentDimension !== previousDimension) {
+          latest_y = 999;
+          previousDimension = currentDimension; // Aktuelle Dimension für den nächsten Tick speichern
+      }
+
+      if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 0 && timer_addon.getScore("only") > 0) {
+        latest_y = 999;
+      }
+
+
+      if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("only") == 1) {
+        const y = Math.floor(player.location.y);
+
+        if (latest_y == 999) {
+          latest_y = y;
         }
-  
-        else if (x < -level) {
-            newX = -level;
-            outsideBorder = true;
-        }
-  
-        if (z > level) {
-            newZ = level;
-            outsideBorder = true;
-        }
-  
-        else if (z < -level) {
-            newZ = -level;
-            outsideBorder = true;
+
+        if (y < latest_y) {
+          world.sendMessage('§l§u[§dOnly§u]§r §l'+ player.name +'§r fall from §by§r '+ latest_y +' to §9y§r '+ y +'!');
+          player.runCommand('scoreboard players set reset_type timer_settings 1');
+          player.runCommand('function timer/system_do_not_use/end_cmo');
+          latest_y == 999;
         }
 
-        // Only
+        if (latest_y !== 999) {
+          latest_y = y;
+        }
+      }
 
-        const currentDimension = player.dimension.id; // Aktuelle Dimension des Spielers abrufen
+      if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("only") == 2) {
+        const y = Math.floor(player.location.y);
 
-        // Überprüfen, ob es einen Dimensionswechsel gab
-        if (currentDimension !== previousDimension) {
-            latest_y = 999;
-            previousDimension = currentDimension; // Aktuelle Dimension für den nächsten Tick speichern
+        if (latest_y == 999) {
+          latest_y = y;
         }
 
-        if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 0 && timer_addon.getScore("only") > 0) {
+        if (y > latest_y) {
+          world.sendMessage('§l§u[§dOnly§u]§r §l'+ player.name +'§r climbed from §by§r '+ latest_y +' to §9y§r '+ y +' up!');
+          player.runCommand('scoreboard players set reset_type timer_settings 1');
+          player.runCommand('function timer/system_do_not_use/end_cmo');
           latest_y = 999;
         }
 
-
-        if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("only") == 1) {
-          const y = Math.floor(player.location.y);
-
-          if (latest_y == 999) {
-            latest_y = y;
-          }
-
-          if (y < latest_y) {
-            world.sendMessage('§l§u[§dOnly§u]§r §l'+ player.name +'§r fall from §by§r '+ latest_y +' to §9y§r '+ y +'!');
-            player.runCommand('scoreboard players set reset_type timer_settings 1');
-            player.runCommand('function timer/system_do_not_use/end_cmo');
-            latest_y == 999;
-          }
-
-          if (latest_y !== 999) {
-            latest_y = y;
-          }
+        if (latest_y !== 999) {
+          latest_y = y;
         }
+        
+      }
 
-        if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("only") == 2) {
-          const y = Math.floor(player.location.y);
+      /* Only Debug
+      player.runCommand('scoreboard players set @s timer_actionbar_time 60');
+      player.runCommand('title @s actionbar §9latest_y:§r '+ latest_y +' | §by:§r '+ latest_y);*/
 
-          if (latest_y == 999) {
-            latest_y = y;
-          }
+      // Block break and place
+      if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("no_block_break") == 1) {
+        world.beforeEvents.playerBreakBlock.subscribe((data) => {
+        data.cancel = true;
+        })
+      }
 
-          if (y > latest_y) {
-            world.sendMessage('§l§u[§dOnly§u]§r §l'+ player.name +'§r climbed from §by§r '+ latest_y +' to §9y§r '+ y +' up!');
-            player.runCommand('scoreboard players set reset_type timer_settings 1');
-            player.runCommand('function timer/system_do_not_use/end_cmo');
-            latest_y = 999;
-          }
-
-          if (latest_y !== 999) {
-            latest_y = y;
-          }
-          
+      if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("no_block_place") == 1) {
+        world.afterEvents.playerPlaceBlock.subscribe((data) => {
+        data.cancel = true;
+        })
+      }
+    
+      // Level = Boader
+      if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("level_equals_border") == 1) {
+        if (outsideBorder && level < 24791) {
+            player.teleport({ x: newX, y: player.location.y, z: newZ });
+            MessageBorder = true;
         }
-
-        /* Only Debug
-        player.runCommand('scoreboard players set @s timer_actionbar_time 60');
-        player.runCommand('title @s actionbar §9latest_y:§r '+ latest_y +' | §by:§r '+ latest_y);*/
-
-        // Block break and place
-        if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("no_block_break") == 1) {
-          world.beforeEvents.playerBreakBlock.subscribe((data) => {
-          data.cancel = true;
-          })
+    
+        if (!outsideBorder && MessageBorder) {
+            player.runCommand('/scoreboard players set @s timer_actionbar_time 60');
+            player.runCommand('title @s actionbar Earne §l§a'+ xp_needed +' XP§r§f to expant the world border');
+            MessageBorder = false;
         }
-  
-        if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("no_block_place") == 1) {
-          world.afterEvents.playerPlaceBlock.subscribe((data) => {
-          data.cancel = true;
-          })
-        }
-      
-        // Level = Boader
-       if (timer_settings.getScore("mode") == 1 && timer_settings.getScore("do_count") == 1 && timer_addon.getScore("level_equals_border") == 1) {
-          if (outsideBorder && level < 24791) {
-              player.teleport({ x: newX, y: player.location.y, z: newZ });
-              MessageBorder = true;
-          }
-      
-          if (!outsideBorder && MessageBorder) {
-              player.runCommand('/scoreboard players set @s timer_actionbar_time 60');
-              player.runCommand('title @s actionbar Earne §l§a'+ xp_needed +' XP§r§f to expant the world border');
-              MessageBorder = false;
-          }
-      
-          if (level !== oldLevel) {
-              if (level > oldLevel && level !== 24791) {
-                  player.runCommand('playsound random.orb @a');
-                  world.sendMessage('§l§c[§bWorld Boader§c]§r The world border expant to §l§a'+ level +' Blocks§r!');
-              }
-      
-              if (level < oldLevel && level !== 24791) {
-                  player.runCommand('playsound note.bassattack @a');
-                  
-                  if (reducedLevel == 1) {
-                      world.sendMessage('§l§c[§bWorld Boader§c]§r The world border reduced by §l§a'+ reducedLevel +' Block§r! Now it is §l§a'+ level +' Blocks§r!');
-                  }
-      
-                  if (reducedLevel !== 1) {
-                      world.sendMessage('§l§c[§bWorld Boader§c]§r The world border reduced by §l§a'+ reducedLevel +' Blocks§r! Now it is §l§a'+ level +' Blocks§r!');
-                  }
-              }
-      
-              if (level == 24791) {
-                  player.runCommand('playsound random.orb @a');
-                  world.sendMessage('§l§c[§bWorld Boader§c]§r The world border is turned off because you have reached the maximum level of §l§a'+ level +'§r!');
-              }
-      
-              oldLevel = level;
-          }
+    
+        if (level !== oldLevel) {
+            if (level > oldLevel && level !== 24791) {
+                player.runCommand('playsound random.orb @a');
+                world.sendMessage('§l§c[§bWorld Boader§c]§r The world border expant to §l§a'+ level +' Blocks§r!');
+            }
+    
+            if (level < oldLevel && level !== 24791) {
+                player.runCommand('playsound note.bassattack @a');
+                
+                if (reducedLevel == 1) {
+                    world.sendMessage('§l§c[§bWorld Boader§c]§r The world border reduced by §l§a'+ reducedLevel +' Block§r! Now it is §l§a'+ level +' Blocks§r!');
+                }
+    
+                if (reducedLevel !== 1) {
+                    world.sendMessage('§l§c[§bWorld Boader§c]§r The world border reduced by §l§a'+ reducedLevel +' Blocks§r! Now it is §l§a'+ level +' Blocks§r!');
+                }
+            }
+    
+            if (level == 24791) {
+                player.runCommand('playsound random.orb @a');
+                world.sendMessage('§l§c[§bWorld Boader§c]§r The world border is turned off because you have reached the maximum level of §l§a'+ level +'§r!');
+            }
+    
+            oldLevel = level;
         }
       }
     }
-
-  if (timer_settings) {
-    is_available = 1;
   }
   system.run(mainTick);
 }
-
 system.run(mainTick);
--- Timer Ultimate/subpacks/s/scripts/main.js
+++ Timer Ultimate/subpacks/s/scripts/main.js
@@ -20,44 +20,35 @@ function getPlayerDimension() {
 
 
 let timer_settings = world.scoreboard.getObjective("timer_settings");
-let is_available = 1;
 
 function mainTick() {
-  // Decet a crash 
-  if (!timer_settings) {
-    is_available = 0;
-  }
 
 
-
-
-  // Sync with Dimension
-  if (is_available === 1) {
 
     const playerDimension = getPlayerDimension();
-    const player = getPlayer();
     if (system.currentTick % 20 === 0) {
-      if (playerDimension !== undefined && timer_settings.getScore("mode") === 0) {
+      
+      // This If query protects the script from crashing when the scoreboard "timer_settings" is deleted.
+      if (world.scoreboard.getObjective("timer_settings") !== undefined) {
 
-        if (playerDimension.id === "minecraft:overworld" && timer_settings.getScore("dimension") !== 0) { 
-          timer_settings.setScore("dimension", 0);
-        }
-        
-        if (playerDimension.id === "minecraft:nether" && timer_settings.getScore("dimension") !== 1) { 
-          timer_settings.setScore("dimension", 1);
-        }
-        // This doesn't work realy well but if you enter the end you get the hidden dimension
-        if (playerDimension.id === "minecraft:the_end" && timer_settings.getScore("dimension") !== 2) { 
-          timer_settings.setScore("dimension", 2);
+        if (playerDimension !== undefined && timer_settings.getScore("mode") === 0) {
+
+          if (playerDimension.id === "minecraft:overworld" && timer_settings.getScore("dimension") !== 0) { 
+            timer_settings.setScore("dimension", 0);
+          }
+          
+          if (playerDimension.id === "minecraft:nether" && timer_settings.getScore("dimension") !== 1) { 
+            timer_settings.setScore("dimension", 1);
+          }
+          // This doesn't work realy well but if you enter the end you get the hidden dimension
+          if (playerDimension.id === "minecraft:the_end" && timer_settings.getScore("dimension") !== 2) { 
+            timer_settings.setScore("dimension", 2);
+          }
         }
       }
     }
 
-  }
 
-  if (timer_settings) {
-    is_available = 1;
-  }
 
 
   system.run(mainTick);
